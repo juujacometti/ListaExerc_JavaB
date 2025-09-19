04bfcb8137947555e47f3a4d033bb0d5199b57e6
juujacometti <julyajacometty@gmail.com>
2025-09-18 21:33:01 -0300

    Lista de exercícios 02 - finalizada

--- Lista 2 - Condicionais/src/Exercicio07.java
+++ Lista 2 - Condicionais/src/Exercicio07.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercicio07 {
@@ -15,7 +16,64 @@ Elabore 02 perguntas com 03 alternativas de resposta cada.
   Pesquise o método da classe String que transforma o texto em minúsculo.
 */
 
+        String alternativa = "";
+        int pergunta;
 
-        String pergunta1 = "";
+        // Verificação de escolha de pergunta
+        while (true) {
+            System.out.println("=== PERGUNTAS E RESPOSTAS ====");
+            System.out.println("\nEscolha entre duas perguntas:");
+            System.out.println("1 - Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?");
+            System.out.println("2 - Quais são as fases da Lua?");
+
+            try {
+                System.out.print("\nInforme qual pergunta você deseja responder (1 ou 2): ");
+                pergunta = leia.nextInt();
+                leia.nextLine(); // limpa o buffer
+
+                // Perguntas disponíveis
+                if (pergunta == 1) {
+                    System.out.println("\nQuanto tempo a Terra demora para dar uma volta completa em torno dela mesma?");
+                    System.out.println("a - Aproximadamente 24 horas");
+                    System.out.println("b - Aproximadamente 7 dias");
+                    System.out.println("c - Aproximadamente 1 ano");
+                }
+                else if (pergunta == 2) {
+                    System.out.println("\nQuais são as fases da Lua?");
+                    System.out.println("a - Nova, crescente, cheia e minguante.");
+                    System.out.println("b - Nova, cheia, minguante e crescendo.");
+                    System.out.println("c - Nova, crescente, cheia, minguante e lua de sangue");
+                }
+                else {
+                    System.out.println("\nDigite apenas 1 ou 2. Tente novamente!\n");
+                    continue;
+                }
+
+                // Verificação de resposta correta
+                System.out.print("\nQual é a resposta correta ('a', 'b' ou 'c')? ");
+                alternativa = leia.nextLine().trim().toLowerCase();
+
+                if (alternativa.isEmpty()) {
+                    System.out.println("\nO campo está vazio. Tente novamente!");
+                    continue;
+                }
+
+                if (pergunta == 1 && alternativa.equals("a") || pergunta == 2 && alternativa.equals("a")) {
+                    System.out.println("\nVocê acertou, pode retirar seu bônus na Shostners and Shostners");
+                }
+                else if (alternativa.equals("b") || alternativa.equals("c")) {
+                    System.out.println("\nVocê não acertou, mas tente novamente numa próxima.");
+                }
+                else {
+                    System.out.println("\nDigite apenas a, b ou c!");
+                }
+
+                break;
+
+            } catch (InputMismatchException e) {
+                System.out.println("\nDigite apenas números inteiros. Tente novamente!\n");
+                leia.nextLine(); // limpa o buffer
+            }
+        }
     }
 }
